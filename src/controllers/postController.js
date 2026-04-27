b47f73549a8bfdfb7c90f5f706998e50b91c0a57
Nishu sharma <nishusharma062060@gmail.com>
2026-04-28 02:08:06 +0530

    Remove comments from postController.js

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,11 +1,11 @@
 const postService = require('../services/postService');
-// ✅ GET ALL POSTS
+
 exports.getAllPosts = async (req, res) => {
   const posts = await postService.getAllPosts();
   res.json(posts);
 };
 
-// ✅ CREATE POST
+
 exports.createPost = async (req, res) => {
   const post = await postService.createPost(req.body);
 
@@ -15,7 +15,7 @@ exports.createPost = async (req, res) => {
   });
 };
 
-// ✅ GET BY ID
+
 exports.getPostById = (req, res) => {
   res.send(`Post ID: ${req.params.id}`);
 };
